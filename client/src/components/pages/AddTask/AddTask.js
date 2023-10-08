dc275afa0407f4fe08161af28e12752497490161
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2023-10-08 23:08:11 +0700

    Connect Service with createTask and PieChart

--- client/src/components/pages/AddTask/AddTask.js
+++ client/src/components/pages/AddTask/AddTask.js
@@ -1,26 +1,89 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import TimePicker from "react-time-picker";
 import "react-time-picker/dist/TimePicker.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./AddTask.css";
 import Navbar from "../Navbar/Navbar";
+import Form from 'react-bootstrap/Form';
+
+// Import Library
+import jwtDecode from "jwt-decode"
+
+// Import API
+import { getAllCategoryTasks } from "../../../services/CategoryTaskService"
+import { createUserTask } from "../../../services/UserTaskService"
 
 function AddTask() {
+  const [name, setName] = useState("")
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [startTime, setStartTime] = useState(null);
   const [endTime, setEndTime] = useState(null);
+  const [category_task_id, setCategory_task_id] = useState(1);
   const colorTextLink = {
     textDecoration: "none",
     color: "#FFFFFF",
   };
 
+  // for call API
+  const [categoryTasks, setCategoryTasks] = useState([]);
+
+  // useNavigate
+  const navigate = useNavigate();
+
+  async function getInitData() {
+    try {
+      const response = await getAllCategoryTasks();
+      setCategoryTasks(response.data)
+    }
+    catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getInitData()
+  }, []);
+
+  async function submitCreateTask(e) {
+    e.preventDefault();
+    const token = localStorage.getItem("token")
+    if (token) {
+      const username = jwtDecode(token).username
+      try {
+        var data = {
+          "name": name,
+          "startDay": startDate,
+          "startHour": startTime,
+          "endDay": endDate,
+          "endHour": endTime,
+          "category_task_id": Number(category_task_id)
+        }
+        const response = await createUserTask(username, data);
+        console.log(response)
+      }
+      catch (error) {
+        console.log(error.message)
+      }
+    }
+    else {
+      console.log("Don't have token")
+      navigate("/")
+    }
+  }
+
+  const categoryOptions = categoryTasks.map((item) => (
+    <option key={item.id} value={item.id}>
+      {item.name}
+    </option>
+  ))
+
   return (
     <div>
-        <Navbar />
+      <Navbar />
       {/* Content */}
       <div className="container vh-100 d-flex justify-content-center align-items-center box-bg">
         {/* Box White */}
@@ -45,6 +108,7 @@ function AddTask() {
                       type="text"
                       className="form-control py-2"
                       placeholder="กรุณากรอกชื่อกิจกรรม"
+                      onChange={(e) => setName(e.target.value)}
                     />
                   </div>
                 </div>
@@ -113,13 +177,34 @@ function AddTask() {
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-12 my-2 d-flex justify-content-center align-items-center">
+                  <p>ประเภทกิจกรรม</p>
+                </div>
+                <div className="row pb-3">
+                  <div className="col-12 d-flex justify-content-start align-items-center">
+                    <Form.Select aria-label="Default select example" onChange={(e) => setCategory_task_id(e.target.value)}>
+                      {categoryOptions}
+                    </Form.Select>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-6 my-2 d-flex justify-content-center align-items-center">
+                    {/* add task button */}
+                    <button
+                      type="button"
+                      className="btn btn-warning px-5 mx-2"
+                    >
+                      <Link to="/index" style={colorTextLink}>กลับหน้าหลัก</Link>
+                    </button>
+                  </div>
+                  <div className="col-6 my-2 d-flex justify-content-center align-items-center">
                     {/* add task button */}
                     <button
                       type="button"
                       className="btn btn-primary px-5 mx-2"
+                      style={colorTextLink}
+                      onClick={(e) => submitCreateTask(e)}
                     >
-                      <Link to="/index" style={colorTextLink}>เพิ่มกิจกรรม</Link>
+                      เพิ่มกิจกรรม
                     </button>
                   </div>
                 </div>
--- client/src/components/pages/Index/ChartExample.js
+++ client/src/components/pages/Index/ChartExample.js
@@ -1,16 +1,94 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
+import { useNavigate } from "react-router-dom";
 
-const ChartExample = () => {
+// Import Library
+import jwtDecode from "jwt-decode"
+
+// Import API
+import { getAllCategoryTasks } from "../../../services/CategoryTaskService"
+import { getAllUserTasksByStatusId } from "../../../services/UserTaskService"
+import { getAllStatus, } from "../../../services/StatusService"
+
+function ChartExample() {
+  // useNavigate
+  const navigate = useNavigate();
+
   const chartRef = useRef(null);
   const chartInstance = useRef(null); // ใช้เก็บอินสแตนซ์ของ Chart
 
+  // for call API
+  const [loading, setLoading] = useState(true)
+  const [categoryTasks, setCategoryTasks] = useState([])
+
+  // without Service
+  const [countAllTask, setCountAllTask] = useState([0, 0, 0, 0])
+
+  async function getInitData() {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      const username = jwtDecode(token).username
+      try {
+        // for getALL status
+        const statusId = []
+        const responseStatus = await getAllStatus();
+        for (let i = 0; i < responseStatus.data.length; i++) {
+          statusId.push(responseStatus.data[i].id)
+        }
+
+        const countData = [] // Collect name with value of Categories
+        // for categoties in Pie Chart
+        const responseCategory = await getAllCategoryTasks();
+        const categoriesName = []
+        const categoriesId = []
+        for (let i = 0; i < responseCategory.data.length; i++) {
+          categoriesName.push(responseCategory.data[i].name)
+          categoriesId.push(responseCategory.data[i].id)
+          countData.push({ name: responseCategory.data[i].name, value: 0 })
+        }
+        setCategoryTasks(categoriesName)
+        
+        // for count in Pie Chart
+        for (let i = 0; i < statusId.length; i++) {
+          const statusID = statusId[i]
+          const responseGetAllUserTasksByStatusId = await getAllUserTasksByStatusId(username, statusID)
+          console.log(responseGetAllUserTasksByStatusId.data)
+          for (let y = 0; y < responseGetAllUserTasksByStatusId.data.length; y++) {
+            const task = responseGetAllUserTasksByStatusId.data[y].task.category_task.name;
+            for (let k = 0; k < countData.length; k++) {
+              if (countData[k].name === task) {
+                countData[k].value = countData[k].value + 1
+                break;
+              }
+            }
+          }
+        }
+        const result = [] // Collect Only Number
+        for (let i = 0; i < countData.length; i++) {
+          result.push(countData[i].value)
+        }
+        setCountAllTask(result)
+
+        // Finish Call API
+        setLoading(false)
+      }
+      catch (error) {
+        console.log(error.message)
+      }
+    }
+    else {
+      console.log("Don't have token")
+      navigate("/")
+    }
+  }
+
   const data = {
-    labels: ["Type1", "Type2", "Type3", "Type4"],
+    labels: !loading ? categoryTasks : ["Type1", "Type2", "Type3", "Type4"],
     datasets: [
       {
         label: "# of Votes",
-        data: [12, 19, 3, 5],
+        data: !loading ? countAllTask : [12, 19, 3, 5],
         borderWidth: 1,
         backgroundColor: [
           "#FF8080",
@@ -23,33 +101,34 @@ const ChartExample = () => {
   };
 
   useEffect(() => {
-  const config = {
-    type: "pie",
-    data: data,
-    options: {
-      plugins: {
-        legend: {
-          onHover: handleHover,
-          onLeave: handleLeave,
+    getInitData()
+    const config = {
+      type: "pie",
+      data: data,
+      options: {
+        plugins: {
+          legend: {
+            onHover: handleHover,
+            onLeave: handleLeave,
+          },
         },
       },
-    },
-  };
+    };
 
-  if (chartInstance.current) {
-    chartInstance.current.destroy(); // ทำลาย Chart เก่า (ถ้ามี)
-  }
-
-  const ctx = chartRef.current.getContext("2d");
-  chartInstance.current = new Chart(ctx, config);
-
-  return () => {
-    // ทำลาย Chart เมื่อ Component ถูก Unmount
     if (chartInstance.current) {
-      chartInstance.current.destroy();
+      chartInstance.current.destroy(); // ทำลาย Chart เก่า (ถ้ามี)
     }
-  };
-}, [data]); // กำหนด dependencies เป็น 'data' เนื่องจากมีการใช้ใน config
+
+    const ctx = chartRef.current.getContext("2d");
+    chartInstance.current = new Chart(ctx, config);
+
+    return () => {
+      // ทำลาย Chart เมื่อ Component ถูก Unmount
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+    };
+  }, [loading]); // กำหนด dependencies เป็น 'data' เนื่องจากมีการใช้ใน config
 
 
   function handleHover(evt, item, legend) {
